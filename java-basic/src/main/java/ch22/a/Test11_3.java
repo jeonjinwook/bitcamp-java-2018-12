4dc56191588bd4bef8925b9fed09a5c33136f68a
jeonjinwook <sambosoft@naver.com>
2019-01-31 18:21:05 +0900

    2019-1-30

---
+++ java-basic/src/main/java/ch22/a/Test11_3.java
@@ -0,0 +1,33 @@
+// java.io.File 클래스 : FileFilter 사용하기
+package ch22.a;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class Test11_3 {
+
+  //  static nested class
+
+  public static void main(String[] args) throws Exception{
+
+
+    File file = new File(".");
+
+    File[] files = file.listFiles(new FileFilter() {
+      @Override
+      public boolean accept(File pathname) {
+        if (pathname.isFile() && pathname.getName().endsWith(".txt"))
+          return true;
+        else
+          return false;
+      }
+    });
+
+    for (File f : files) {
+      System.out.printf("%s %12d %s\n",
+          f.isDirectory() ? "d" : "-",
+              f.length(),
+              f.getName());
+    }
+  }
+}

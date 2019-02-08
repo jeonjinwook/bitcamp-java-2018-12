b8e50f28e8c374ca3c0dfe17076119976a086756
jeonjinwook <sambosoft@naver.com>
2019-02-08 10:04:15 +0900

    2019-2-8

---
+++ java-basic/src/main/java/design_pattern/observer/before/Car.java
@@ -0,0 +1,15 @@
+package design_pattern.observer.before;
+
+public class Car {
+	public void start() {
+		System.out.println("시동을 건다.");
+	}
+
+	public void run() {
+		System.out.println("달린다");
+	}
+
+	public void stop() {
+		System.out.println("시동을 끈다.");
+	}
+}

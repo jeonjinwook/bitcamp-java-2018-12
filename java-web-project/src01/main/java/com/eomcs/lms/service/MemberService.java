bbb8b02387c5c60dbcedc9c263623cab9bbbe014
jeonjinwook <sambosoft@naver.com>
2019-04-01 18:09:24 +0900

    인클루딩과 포워딩방식

---
+++ java-web-project/src01/main/java/com/eomcs/lms/service/MemberService.java
@@ -0,0 +1,20 @@
+// 프록시 패턴 적용 - MemberDao에서 인터페이스를 추출한다.
+package com.eomcs.lms.service;
+
+import java.util.List;
+import com.eomcs.lms.domain.Member;
+
+public interface MemberService {
+  int add(Member member);
+  List<Member> list(String keyword);
+  Member get(int no);
+  int update(Member member);
+  int delete(int no);
+}
+
+
+
+
+
+
+
